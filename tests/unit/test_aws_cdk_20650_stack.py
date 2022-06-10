89e5ee1250a745cf1adee7efffc8e1713d365d95
Philip Colmer <philip.colmer@linaro.org>
2022-06-10 11:12:09 +0100

    First commit

---
+++ tests/unit/test_aws_cdk_20650_stack.py
@@ -0,0 +1,19 @@
+# from aws_cdk import (
+#         core,
+#         assertions
+#     )
+
+# from aws_cdk_20650.aws_cdk_20650_stack import AwsCdk20650Stack
+
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in aws_cdk_20650/aws_cdk_20650_stack.py
+def test_sqs_queue_created():
+#     app = core.App()
+#     stack = AwsCdk20650Stack(app, "aws-cdk-20650")
+#     template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
+  pass
